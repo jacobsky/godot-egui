4b6f128a443afbe33e5f3fea85721ecc54b47754
Jacobsky <cael.jacobsen@gmail.com>
2021-08-27 12:49:21 +0900

    WIP: Creating an EGUI Styler addon by using the egui settings window as a test.

--- example_project/src/lib.rs
+++ example_project/src/lib.rs
@@ -1,6 +1,9 @@
 use gdnative::prelude::*;
 use godot_egui::GodotEgui;
 
+mod styler;
+pub use styler::GodotEguiStyler;
+
 pub fn load_texture(path: &str) -> Ref<Texture> {
     let loader = ResourceLoader::godot_singleton();
     loader.load(path, "Texture", false).expect("Texture found").cast().expect("Is texture")
@@ -189,6 +192,7 @@ impl GodotEguiExample {
 
 fn init(handle: InitHandle) {
     handle.add_tool_class::<GodotEguiExample>();
+    handle.add_tool_class::<GodotEguiStyler>();
     godot_egui::register_classes(handle);
 }
 
